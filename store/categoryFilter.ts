a558d11c860b703f7015246039cd808956c86ee1
NguyenVanCanh <ngcanh1236@gmail.com>
2024-12-25 23:29:45 +0700

    feat: filter store

---
+++ store/categoryFilter.ts
@@ -0,0 +1,52 @@
+import { create } from "zustand"
+
+interface CategoryFilterState {
+  isOpen: boolean
+  available: boolean
+  brands: string[]
+  priceFrom: number
+  priceTo: number
+  star: number
+  open: () => void
+  close: () => void
+  toggleBrand: (brand: string) => void
+  setPriceFrom: (price: number) => void
+  setPriceTo: (price: number) => void
+  setStar: (star: number) => void
+  toggleAvailabel: () => void
+}
+
+export const useCategoryFilterStore = create<CategoryFilterState>()((set) => ({
+  isOpen: false,
+  available: false,
+  brands: [],
+  priceFrom: 0,
+  priceTo: 100,
+  star: 5,
+  open: () => set(() => ({ isOpen: true })),
+  close: () => set(() => ({ isOpen: false })),
+  toggleBrand: (brand: string) =>
+    set((state) => ({
+      brands: state.brands.includes(brand)
+        ? [...state.brands.filter((b) => b != brand)]
+        : [...state.brands, brand],
+    })),
+  setPriceFrom: (price: number) => set(() => ({ priceFrom: price })),
+  setPriceTo: (price: number) => set(() => ({ priceTo: price })),
+  setStar: (star: number) => set(() => ({ star })),
+  toggleAvailabel: () => set((state) => ({ available: !state.available })),
+}))
+
+
+export const allBrands = [
+  "Nestlé",
+  "Coca-Cola",
+  "PepsiCo",
+  "Unilever",
+  "Kraft Heinz",
+  "Danone",
+  "Mars",
+  "Mondelez International",
+  "Tyson Foods",
+  "General Mills",
+]
