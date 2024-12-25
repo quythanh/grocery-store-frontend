2c646a365ea30103fdfbbc6dab570501fda87700
NguyenVanCanh <ngcanh1236@gmail.com>
2024-12-25 10:52:01 +0700

    feat: integrate gluestack/ui, React Query and Zustand and config prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Stack } from "expo-router"
+
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider"
+
+import "@/global.css"
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider mode="light">
+      <QueryClientProvider client={queryClient}>
+        <Stack />
+      </QueryClientProvider>
+    </GluestackUIProvider>
+  )
 }
