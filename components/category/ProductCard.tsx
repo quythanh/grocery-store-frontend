5b948320e79b8dcae63a2cc1c7a3acaf64a81c65
NguyenVanCanh <ngcanh1236@gmail.com>
2024-12-27 14:42:05 +0700

    feat(category): adjust quantity to cart popover

--- components/category/ProductCard.tsx
+++ components/category/ProductCard.tsx
@@ -1,12 +1,24 @@
-import React from "react"
-import { Plus, ShoppingCart, Weight } from "lucide-react-native"
+import React, { useState } from "react"
+import {
+  MinusIcon,
+  Plus,
+  PlusIcon,
+  ShoppingCart,
+  Weight,
+} from "lucide-react-native"
 
-import { Button, ButtonIcon } from "../ui/button"
+import { Button, ButtonIcon, ButtonText } from "../ui/button"
 import { Card } from "../ui/card"
 import { Heading } from "../ui/heading"
 import { HStack } from "../ui/hstack"
 import { Icon } from "../ui/icon"
 import { Image } from "../ui/image"
+import {
+  Popover,
+  PopoverBackdrop,
+  PopoverBody,
+  PopoverContent,
+} from "../ui/popover"
 import { Text } from "../ui/text"
 import { VStack } from "../ui/vstack"
 
@@ -24,6 +36,14 @@ const ProductCard = ({
   className?: string
   product: Product
 }) => {
+  const [quantityToCart, setQuantityToCart] = useState(1)
+  const handleAddToCart = () => {}
+
+  const adjustQuantity = (quantity: number) => {
+    if (quantityToCart + quantity < 1) return
+    setQuantityToCart(quantityToCart + quantity)
+  }
+
   return (
     <Card className={`p-3 rounded-xl w-full ${className || ""}`}>
       <Image
@@ -46,10 +66,54 @@ const ProductCard = ({
             <Text>{product.qty}gram</Text>
           </HStack>
         </VStack>
-        <Button size="sm" className="bg-mainGreen active:!bg-green-700">
-          <ButtonIcon as={Plus} />
-          <ButtonIcon as={ShoppingCart} />
-        </Button>
+
+        <Popover
+          placement="top right"
+          size="md"
+          trigger={(triggerProps) => {
+            return (
+              <Button
+                size="sm"
+                className="bg-mainGreen active:!bg-green-700"
+                {...triggerProps}
+              >
+                <ButtonIcon as={Plus} />
+                <ButtonIcon as={ShoppingCart} />
+              </Button>
+            )
+          }}
+        >
+          <PopoverBackdrop />
+          <PopoverContent className="p-2 mb-2">
+            <PopoverBody>
+              <HStack className="gap-2 items-center shadow-md justify-between mb-2 px-2">
+                <Button
+                  size="xs"
+                  variant="outline"
+                  className="size-6 rounded-full"
+                  onPress={adjustQuantity.bind(this, -1)}
+                >
+                  <ButtonIcon as={MinusIcon} />
+                </Button>
+                <Text>{quantityToCart}</Text>
+                <Button
+                  size="xs"
+                  variant="outline"
+                  className="size-4 rounded-full"
+                  onPress={adjustQuantity.bind(this, 1)}
+                >
+                  <ButtonIcon as={PlusIcon} />
+                </Button>
+              </HStack>
+              <Button
+                onPress={handleAddToCart}
+                className="bg-mainGreen active:!bg-green-700"
+              >
+                <ButtonText>Add to cart</ButtonText>
+              </Button>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </HStack>
     </Card>
   )
