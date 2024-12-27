ab474067a733f9cf1a794ba2986595f498043932
quythanh <quythanh.dev@gmail.com>
2024-12-27 10:45:01 +0700

    feat(cart): add shopping cart overview

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,29 +1,55 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 const CartScreen = () => {
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedText style={styles.title}>Cart</ThemedText>
-      <ThemedText style={styles.text}>You can view your cart here</ThemedText>
-    </ThemedView>
-  );
+    return (
+        <ParallaxScrollView
+            headerImage={
+                <View style={styles.headerContainer}>
+                    <ThemedText style={styles.headerText}>Shopping Cart</ThemedText>
+                    <ThemedText style={styles.headerSubText}>A total of 2 pieces</ThemedText>
+                </View>
+            }
+            headerBackgroundColor={{ light: "#64A86B", dark: "#1D3D47" }}
+        >
+        </ParallaxScrollView>
+    );
 };
 
 export default CartScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  text: {
-    fontSize: 16,
-  },
+    headerContainer: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: "flex",
+        gap: 8,
+        paddingHorizontal: 24
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: 500,
+        width: "100%",
+        color: '#FFFFFF',
+    },
+    headerSubText: {
+        fontSize: 16,
+        color: '#FFFFFF',
+        width: "100%",
+        textAlign: "right"
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    text: {
+        fontSize: 16,
+    },
 });
