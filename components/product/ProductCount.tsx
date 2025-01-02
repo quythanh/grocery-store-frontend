6df4354e48758593283a3c756c52b4c00383c0ef
NguyenVanCanh <ngcanh1236@gmail.com>
2025-01-02 11:17:58 +0700

    feat: add ProductCount component for managing item count in cart

---
+++ components/product/ProductCount.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { MinusIcon, PlusIcon } from "lucide-react-native"
+
+import { ProductToCart } from "@/app/product/[id]"
+
+import { Button, ButtonIcon } from "../ui/button"
+import { Heading } from "../ui/heading"
+import { HStack } from "../ui/hstack"
+import { Text } from "../ui/text"
+
+const ProductCount = ({
+  handleChange,
+  count,
+}: {
+  handleChange: (key: keyof ProductToCart, value: number) => void
+  count: number
+}) => {
+  return (
+    <HStack className="items-center gap-2">
+      <Heading size="sm">Item count</Heading>
+
+      <HStack className="gap-2 items-center shadow-md justify-between mb-2 px-2 w-40 mt-4">
+        <Button
+          size="xs"
+          variant="outline"
+          className="size-6 rounded-full"
+          onPress={() => handleChange("count", count - 1)}
+        >
+          <ButtonIcon as={MinusIcon} />
+        </Button>
+        <Text>{count}</Text>
+        <Button
+          size="xs"
+          variant="outline"
+          className="size-4 rounded-full"
+          onPress={() => handleChange("count", count + 1)}
+        >
+          <ButtonIcon as={PlusIcon} />
+        </Button>
+      </HStack>
+    </HStack>
+  )
+}
+
+export default ProductCount
