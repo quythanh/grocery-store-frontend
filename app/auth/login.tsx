50e36ac6e5f489ecb3fdaf3b792a72ddcb4a7cf5
pdmv <2151013110vuong@ou.edu.vn>
2025-01-03 10:46:10 +0700

    chore: handle back button on log in page to back to previous page

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -19,7 +19,7 @@ const Login = () => {
   const { hidePassword, handleHidePassword } = useHidePasswordStore()
 
   const handleBack = () => {
-    route.navigate("/")
+    route.back()
   }
 
   return (
