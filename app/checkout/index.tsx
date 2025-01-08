913cdb0dd7b41543e6d80d54d4d51e5123bdbfa9
NguyenVanCanh <ngcanh1236@gmail.com>
2025-01-08 15:18:22 +0700

    style: re-style for ios

--- app/checkout/index.tsx
+++ app/checkout/index.tsx
@@ -5,7 +5,7 @@ import {
   PackageCheck,
   Truck,
 } from "lucide-react-native"
-import { View } from "react-native"
+import { Platform, SafeAreaView, View } from "react-native"
 
 import { Button, ButtonSpinner, ButtonText } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
@@ -54,8 +54,44 @@ const Checkout = () => {
   const [currentStep, setCurrentStep] = useState(0)
   const [loading, setLoading] = useState(false)
   const toast = useToast()
+
+  const handlePrev = () => {
+    if (currentStep <= 0) {
+      return
+    }
+    setCurrentStep(currentStep - 1)
+  }
+
+  const handleNext = () => {
+    if (currentStep < 2) {
+      setCurrentStep(currentStep + 1 > 2 ? 0 : currentStep + 1)
+      return
+    }
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      toast.show({
+        placement: "top right",
+        render: () => (
+          <View className="px-6 pt-10">
+            <Toast
+              action="success"
+              variant="outline"
+              className="w-full px-10 border border-mainGreen"
+            >
+              <ToastTitle>Notification</ToastTitle>
+              <ToastDescription>Place order successfully!!</ToastDescription>
+            </Toast>
+          </View>
+        ),
+      })
+    }, 3000)
+  }
+
   return (
-    <View className="pt-8 flex-1 pb-2">
+    <SafeAreaView
+      className={`pt-8 flex-1 ${Platform.OS === "ios" ? "pb-8" : "pb-4"}`}
+    >
       <HStack className="bg-mainGreen px-6 py-3 gap-3 items-center mb-6">
         <BackButton />
         <Heading className="text-typography-0">Checkout</Heading>
@@ -64,43 +100,25 @@ const Checkout = () => {
       <CheckoutProgress current={currentStep} />
 
       <View className="flex-1 px-8 mt-4">{steps[currentStep].content}</View>
-
+      {currentStep ? (
+        <Button
+          variant="outline"
+          className="border-mainGreen active:!border-green-700 mx-8 rounded-full bg-background-0"
+          size="lg"
+          onPress={handlePrev}
+        >
+          <ButtonText className="text-mainGreen active:!text-green-700 ">Previous</ButtonText>
+        </Button>
+      ) : null}
       <Button
-        className="bg-mainGreen active:!bg-green-700 mx-8 rounded-full"
+        className="bg-mainGreen active:!bg-green-700 mx-8 rounded-full mt-2"
         size="lg"
-        onPress={() => {
-          if (currentStep < 2) {
-            setCurrentStep(currentStep + 1 > 2 ? 0 : currentStep + 1)
-            return
-          }
-
-          setLoading(true)
-          setTimeout(() => {
-            setLoading(false)
-            toast.show({
-              placement: "top right",
-              render: () => (
-                <View className="px-6 pt-10">
-                  <Toast
-                    action="success"
-                    variant="outline"
-                    className="w-full px-10 border border-mainGreen"
-                  >
-                    <ToastTitle>Notification</ToastTitle>
-                    <ToastDescription>
-                      Place order successfully!!
-                    </ToastDescription>
-                  </Toast>
-                </View>
-              ),
-            })
-          }, 3000)
-        }}
+        onPress={handleNext}
       >
         {loading && <ButtonSpinner color={"#ccc"} />}
         <ButtonText>{currentStep == 2 ? "Place Order" : "Next"}</ButtonText>
       </Button>
-    </View>
+    </SafeAreaView>
   )
 }
 
