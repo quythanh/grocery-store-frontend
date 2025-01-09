cd388a9c782417cf28c974ddedfb4c9ef9c5e3ac
NguyenVanCanh <ngcanh1236@gmail.com>
2025-01-09 11:14:05 +0700

    feat: implement FavoriteScreen with ProductFavouriteCard and pagination

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,35 +1,53 @@
-import { StyleSheet } from "react-native"
+import { useState } from "react"
+import { Platform, ScrollView, View } from "react-native"
 
-import { ThemedText } from "@/components/ThemedText"
-import { ThemedView } from "@/components/ThemedView"
-import { Text } from "@/components/ui/text"
-import { Link } from "expo-router"
+import { Box } from "@/components/ui/box"
+import { Grid, GridItem } from "@/components/ui/grid"
+import { Heading } from "@/components/ui/heading"
+import { HStack } from "@/components/ui/hstack"
+import groceryProducts from "@/components/category/data"
+import BackButton from "@/components/common/BackButton"
+import Pagination from "@/components/common/Pagination"
+import { Product } from "@/components/product/ProductCard"
+import ProductFavouriteCard from "@/components/product/ProductFavouriteCard"
 
 const FavoriteScreen = () => {
+  const [data, setData] = useState(groceryProducts)
+  const handleFavourite = (product: Product) => {
+    if (data.includes(product)) setData(data.filter((p) => p.id != product.id))
+  }
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText style={styles.title}>Favorite</ThemedText>
-      <ThemedText style={styles.text}>You can favorite items here</ThemedText>
-      <Link href="/checkout">
-        <Text>Checkout</Text>
-      </Link>
-    </ThemedView>
+    <View className={`flex-1 ${Platform.OS === "ios" ? "pb-8" : "pb-4"}`}>
+      <HStack
+        className={`bg-mainGreen px-6 py-3 gap-3 items-center mb-6 ${Platform.OS === "ios" ? "pt-16" : "pt-12"}`}
+      >
+        <BackButton />
+        <Heading className="text-typography-0">Favourite</Heading>
+      </HStack>
+
+      <ScrollView>
+        <Grid
+          className="gap-2 px-4"
+          _extra={{
+            className: "col-span-12",
+          }}
+        >
+          {data.map((product, i) => (
+            <GridItem
+              key={i}
+              _extra={{
+                className: "col-span-6 ",
+              }}
+            >
+              <ProductFavouriteCard product={product} onPress={handleFavourite.bind(this, product)} />
+            </GridItem>
+          ))}
+        </Grid>
+        <Pagination first={1} last={4} current={3} onChange={() => {}} />
+        <Box className={Platform.OS === "ios" ? "h-28" : "h-4"}></Box>
+      </ScrollView>
+    </View>
   )
 }
 
 export default FavoriteScreen
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  text: {
-    fontSize: 16,
-  },
-})
