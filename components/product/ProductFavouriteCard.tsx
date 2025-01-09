9974922c99da7ca4724186e55162fc45c13b1496
NguyenVanCanh <ngcanh1236@gmail.com>
2025-01-09 11:13:52 +0700

    feat: add ProductFavouriteCard component

---
+++ components/product/ProductFavouriteCard.tsx
@@ -0,0 +1,74 @@
+import { Link } from "expo-router"
+import {
+  Bookmark,
+  Weight,
+} from "lucide-react-native"
+
+import { Box } from "../ui/box"
+import { Button, ButtonIcon, ButtonText } from "../ui/button"
+import { Card } from "../ui/card"
+import { Heading } from "../ui/heading"
+import { HStack } from "../ui/hstack"
+import { Icon } from "../ui/icon"
+import { Image } from "../ui/image"
+import { Text } from "../ui/text"
+import { VStack } from "../ui/vstack"
+import { Product } from "./ProductCard"
+
+const ProductFavouriteCard = ({
+  product,
+  onPress,
+  className,
+}: {
+  className?: string
+  product: Product
+  onPress: () => void
+}) => {
+  if (!product)
+    return <Box className="bg-gray-200 animate-pulse rounded-xl h-52"></Box>
+
+  return (
+    <Link
+      href={{
+        pathname: "/product/[id]",
+        params: {
+          id: product.id,
+        },
+      }}
+    >
+      <Card className={`p-3 rounded-xl w-full h-full ${className || ""}`}>
+        <Image
+          source={{
+            uri: product.image,
+          }}
+          className="mb-2 w-full h-36 rounded-md"
+          alt="image"
+        />
+        <Heading size="md" className="h-14 line-clamp-2 overflow-ellipsis">
+          {product.name}
+        </Heading>
+        <HStack className="items-end justify-between flex-1">
+          <VStack>
+            <Text className="flex-1 font-bold text-lg">
+              {product.price}.000đ
+            </Text>
+            <HStack className=" items-center gap-1">
+              <Icon as={Weight} size={"sm"} />
+              <Text>{product.qty}gram</Text>
+            </HStack>
+          </VStack>
+
+          {/* heart */}
+          <Button variant="link" size="lg" onPress={onPress}>
+            <ButtonIcon
+              as={Bookmark}
+              className="fill-yellow-500 text-yellow-500"
+            />
+          </Button>
+        </HStack>
+      </Card>
+    </Link>
+  )
+}
+
+export default ProductFavouriteCard
