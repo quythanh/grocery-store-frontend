b33b4e000470058f81a18887297bd8bb86cbd58d
pdmv <2151013110vuong@ou.edu.vn>
2025-01-10 16:16:12 +0700

    fix: user can slide back when in tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -87,7 +87,10 @@ export default function RootLayout() {
                   headerShown: false,
                 }}
               />
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="(tabs)"
+                options={{ headerShown: false, gestureEnabled: false }}
+              />
               <Stack.Screen name="+not-found" />
             </Stack>
             <StatusBar style="auto" />
