6560f4b1809fe46ae369e49a695d3d09b7fae431
quythanh <quythanh.dev@gmail.com>
2025-01-15 11:28:59 +0700

    chore: add "add item to cart" mutation

--- api/graphqlString/cart.ts
+++ api/graphqlString/cart.ts
@@ -44,6 +44,22 @@ export const GET_CUSTOMER_CART = gql`
     }
 `
 
+export const ADD_ITEM_TO_CART = gql`
+    mutation AddItemToCart($cartId: String!, $itemSku: String!, $quantity: Float!) {
+        addProductsToCart(
+            cartId: $cartId
+            cartItems: {
+                sku: $itemSku,
+                quantity: $quantity
+            }
+        ) {
+            cart {
+                total_quantity
+            }
+        }
+    }
+`
+
 export const UPDATE_CART_ITEM = gql`
     mutation UpdateCartItems($cartId: String!, $cartItemUid: ID!, $quantity: Float!) {
         updateCartItems(
